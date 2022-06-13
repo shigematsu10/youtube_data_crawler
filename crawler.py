5d1ebe736048c46f4ec2469de0ab4af414308b2e
shigematsu10 <shigematsu10kuma@gmail.com>
2022-06-13 11:22:55 +0900

    update

--- crawler.py
+++ crawler.py
@@ -15,10 +15,10 @@ from src.get_video_information import Video
 if __name__ == '__main__':
     # Enter your youtube_api_key
     YOUTUBE_API_KEY = 'AIzaSyDkk2CIROIS5iDXxXktsDWEY_2JY4k4GUI'
-    youtube = build('youtube',' v3', developerKey=YOUTUBE_API_KEY)
+    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
     search_word = input('Enter Searching key words : ')
 
     video_info_processor = Video(youtube = youtube)
     result = video_info_processor.get_video_info(part='snippet', q=search_word, regionCode='JP', num=20)
-
+    
     result.to_csv(f'./result/result_of_{search_word}.csv', index=False)
